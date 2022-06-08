62197e6cc3ab1f8cbc22c4deef16f9941330f562
Nouvoh GK <gkelly@nouvoh.com>
2022-06-07 21:23:24 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace VS2022CalendarApp2022.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
